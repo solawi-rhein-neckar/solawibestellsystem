f9182ce068db648bb61fa933111ebf0aec743036
regenduft <fjgh@regenduft.de>
2019-03-16 19:42:57 +0100

    add javascript frontend files

---
+++ table.js
@@ -0,0 +1,165 @@
+function showTableEditor(response, path) {
+	showTable(response, path, true);
+}
+function showTable(response, path, editable) {
+	var table = document.getElementById(editable ? 'tableEdit' : 'table');
+	table.innerHTML = '';
+	if (editable) {
+		window.sbsEditTablePath = path;
+		window.sbsEditTable = path.match(/[^\/]*/)[0];
+	} else {
+		window.sbsViewTablePath = path;
+	}
+	var keys = null;
+	if (response.length == 0 && editable) {
+		var tr = document.createElement("TR");
+		var td = document.createElement("TD");
+		table.appendChild(tr);
+		tr.appendChild(td);
+		var btn = document.createElement('BUTTON');
+		td.appendChild(btn);
+		
+		btn.addEventListener('click', createFuncAddNew(window.sbsEditTable == 'BenutzerZusatzBestellung' ? ['Benutzer_ID', 'Produkt_ID', 'Woche'] : window.sbsEditTable == 'KorbInhaltWoche' ? ['KorbInhalt_ID'] : window.sbsEditTable == 'KorbInhalt' ? ['Korb_ID', 'Produkt_ID'] : ['Name']));
+		btn.innerText = 'ADD';
+	}
+	for (var i = 0; i < response.length; i++) {
+		if(!keys) {
+			keys = Object.keys(response[i]).sort(function(a,b){return a == 'ID' ? -1 : b == 'ID' ? 1 : a == b ? 0 : a > b ? 1 : -1});
+			
+			var tr = document.createElement("TR");
+			table.appendChild(tr);
+			for (var j = 0; j < keys.length; j++) {
+				var td = document.createElement("TD");
+				td.className='col_'+keys[j];
+				tr.appendChild(td);
+				if (j == 0 && editable) {
+					var btn = document.createElement('BUTTON');
+					btn.addEventListener('click', createFuncAddNew(keys));
+					btn.innerText = 'ADD';
+					td.appendChild(btn);
+				} else {
+					td.innerText = keys[j];
+				}
+			}
+			if (editable) {
+				var delTd = document.createElement("TD");
+				delTd.innerText="del";
+				tr.appendChild(delTd);
+			}
+		}
+
+		for (var j = 0; j < keys.length; j++) {
+			var tr = document.createElement("TR");
+			table.appendChild(tr);
+			for (var j = 0; j < keys.length; j++) {
+				var td = document.createElement("TD");
+				td.className='col_'+keys[j];
+				tr.appendChild(td);
+				
+				var div = document.createElement("DIV");
+				div.id = "span_" + path + "_" + i + "_" + j;
+				div.innerText = response[i][keys[j]] || '-';
+				if (editable && keys[j] != 'ID' && keys[j] != 'AenderBenutzer_ID' && keys[j] != 'AenderZeitpunkt' && keys[j] != 'ErstellZeitpunkt') {
+					div.addEventListener('click', showEditor);
+					div.style.cursor = "pointer";
+					div.title = "click to edit!";
+				}
+				div.dataId = response[i]["ID"];
+				div.dataKey = keys[j];
+				
+				td.appendChild(div);
+			}
+			if (editable) {
+				var delTd = document.createElement("TD");
+				var delBtn = document.createElement("BUTTON");
+				delBtn.innerText="del";
+				delBtn.dataId = response[i]["ID"];
+				delBtn.addEventListener('click', function(event) {
+					if (confirm('Really delete ' + window.sbsEditTable + "/" + event.target.dataId)) {
+						deleteAjax(window.sbsEditTable + "/" + event.target.dataId, function(){getAjax(window.sbsEditTablePath, showTableEditor);if (window.sbsViewTablePath)getAjax(window.sbsViewTablePath, showTable);});
+					}
+				});
+				delTd.appendChild(delBtn);
+				tr.appendChild(delTd);
+			}
+		}
+	}
+}
+
+function createFuncAddNew(keys) {
+	return function() {
+		var edit = resetEditor("Add new " + window.sbsEditTable);
+		for (var j = 0; j < keys.length; j++) {
+			if (keys[j] != 'ID' && keys[j] != 'ErstellZeitpunkt' && keys[j] != 'AenderBenutzer_ID' && keys[j] != 'AenderZeitpunkt') {
+				var inp = document.createElement("INPUT");
+				inp.className = 'editor inp_' + keys[j];
+				if (keys[j] == 'Benutzer_ID') {
+					inp.value = sbsUser.ID;
+				} else if (keys[j] == 'Woche' && sbsSelectedWeek) {
+					inp.value = sbsSelectedWeek;
+				}
+				inp.dataKey = keys[j];
+				inp.placeholder = keys[j];
+				edit.appendChild(inp);
+			}
+		}
+		finishEditor(edit);
+	}
+}
+
+function showEditor(event) {
+	var edit = resetEditor("ID " + event.target.dataId + ": " + event.target.dataKey + " ");
+
+	var inp = document.createElement("INPUT");
+	inp.value = event.target.innerText;
+	inp.className = 'editor inp_' + event.target.dataKey;
+	inp.dataId = event.target.dataId;
+	inp.dataKey = event.target.dataKey;
+	inp.addEventListener('keypress', saveEditorInputs);
+	/*inp.addEventListener('change', saveEditorInputs);*/			
+	
+	edit.appendChild(inp);
+	finishEditor(edit);
+}
+
+function resetEditor(label) {
+	var edit = document.getElementById('editor');
+	var label = document.createElement("SPAN");
+	label.innerText = label;
+	edit.appendChild(label);
+	while (edit.firstChild) edit.removeChild(edit.firstChild);
+	show('blockui_edit');
+	return edit;
+}
+
+function finishEditor(edit) {
+	var btn = document.createElement("BUTTON");
+	btn.innerText="Save";
+	btn.style['margin-left'] = '5px';
+	btn.style['margin-right'] = '10px';
+	btn.addEventListener('click', saveEditorInputs);
+	
+	var btn2 = document.createElement("BUTTON");
+	btn2.innerText="Cancel";
+	btn2.addEventListener('click', function(){hide('blockui_edit');});
+	edit.appendChild(btn);
+	edit.appendChild(btn2);
+}
+
+function saveEditorInputs(event2) {
+	if (event2.keyCode == null /*blur*/ || event2.keyCode == 13 /*enter*/) {
+		event2.target.disabled='disabled';
+		var data = {};
+		var editor = document.getElementById('editor');
+		for (var i = 0; i < editor.children.length; i++) {
+			var inpEle = editor.children[i];
+			var id;
+			if (inpEle.tagName == 'INPUT') {
+				data[inpEle.dataKey] = inpEle.value;
+				id = id || inpEle.dataId;
+			}
+		}
+		postAjax(window.sbsEditTable + (id ? '/'+id : ''), data, function(){getAjax(window.sbsEditTablePath, showTableEditor);if (window.sbsViewTablePath)getAjax(window.sbsViewTablePath, showTable);});
+		hide('blockui_edit');
+	}
+}
---
+++ util.js
@@ -0,0 +1,93 @@
+function postAjax(path, data, success, method) {
+	var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+	xhr.open(method || (data ? 'POST' : 'GET'), '/cgi-bin/resql.pl/' + path);
+	xhr.onreadystatechange = function() {
+		hide('blockui_get');
+		hide('blockui_post');
+		if (xhr.readyState>3 && xhr.status==200) { 
+			var result = JSON.parse(xhr.responseText);
+			if (result.reason || result.result) {
+				var msgs = document.getElementById('messages');
+				var msg = document.createElement("DIV");
+				msg.style['white-space']='nowrap';
+				msg.style.overflow='hidden';
+				msg.style['text-overflow']='ellipsis';
+				msg.title = result.reason;
+				msg.innerText = (msgs.children.length + 1) + ": (" + result.result + ") " + (result.reason || (result.result ? "success" : "error"));
+				msgs.insertBefore(msg, msgs.firstChild);
+			}
+			success(result, path); 
+		}
+	};
+	xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	if (data) {
+		show('blockui_post');
+		xhr.send(JSON.stringify(data));
+	} else  {
+		show('blockui_get');
+		xhr.send();
+	}
+	return xhr;
+}
+function getAjax(path, success) {
+	return postAjax(path, null, success);
+}
+function deleteAjax(path, success) {
+	return postAjax(path, null, success, 'DELETE');
+}
+
+function show(id) {
+	var ele = document.getElementById(id);
+	if (ele) ele.style.display='block';
+}
+function hide(id) {
+	var ele = document.getElementById(id);
+	if (ele) ele.style.display='none';
+}
+function setContent(id, text) {
+	var ele = document.getElementById(id);
+	if (ele) ele.innerText=text;
+}
+
+
+// This script is released to the public domain and may be used, modified and
+// distributed without restrictions. Attribution not necessary but appreciated.
+// Source: https://weeknumber.net/how-to/javascript
+
+// Returns the ISO week of the date.
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
+// Returns the four-digit year corresponding to the ISO week of the date.
+Date.prototype.getWeekYear = function() {
+  var date = new Date(this.getTime());
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  return date.getFullYear();
+}
+
+function addWeek(pWeek, count) {
+	year = Math.floor(pWeek);
+	week = (pWeek * 100 + count);
+	if (week % 100 > weekCount(year)) {
+		week += 101 - (week % 100);
+	} else if (week % 100 == 0) {
+		week -= 100 - weekCount(year -1 );
+	}
+	week = week / 100;
+	return week.toFixed(2);
+}
+	
+function weekCount(year) {
+	date = new Date(year, 11 /*month index 0 based - dec = 11*/, 31, 12 /*mid of day*/, 0);
+	return date.getWeek() != 53 ? 52 : 53;
+}
