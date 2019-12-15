11e0138cd55b4f77f3b8b0a626bb1b815b7b15ce
Juergen Czerny <mail@jczerny.de>
2019-12-15 16:38:16 +0100

    ci(setup): test github action

--- util.spec.js
+++ util.spec.js
@@ -3,4 +3,8 @@ describe('first test suite' ,  () => {
     it('first test', () => {
         expect(true).toBe(true);
     });
+
+    it('CI test', () => {
+        expect(true).toBe(true);
+    });
 });
