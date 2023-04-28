eeda5ad8fccc0af706ee5d962468923753bd4a88
Florian Jostock <fjgh@regenduft.de>
2023-04-28 15:57:05 +0200

    Week Table Use for Filling ModulInhaltWoche Example

--- model/procedures/FillWeekTable.sql
+++ model/procedures/FillWeekTable.sql
@@ -17,3 +17,14 @@ BEGIN
 
   END WHILE;
 END
+
+/*
+
+Week Table Use Example:
+
+INSERT INTO ModulInhaltWoche (ModulInhalt_ID, Woche, Anzahl, Depot_ID) SELECT ModulInhalt.ID, Woche.Woche, 1, Depot.ID FROM ModulInhalt ,Depot ,Woche WHERE ModulInhalt.ID in (25,26,27,28,29) AND Depot.ID in (2,3,1,7) AND Woche.Woche >= '2023.18' AND Woche.DonnerstagDesMonats=1;
+INSERT INTO ModulInhaltWoche (ModulInhalt_ID, Woche, Anzahl, Depot_ID) SELECT ModulInhalt.ID, Woche.Woche, 1, Depot.ID FROM ModulInhalt ,Depot ,Woche WHERE ModulInhalt.ID in (25,26,27,28,29) AND Depot.ID in  (14,17,5,9,18) AND Woche.Woche >= '2023.18' AND Woche.DonnerstagDesMonats=2;
+INSERT INTO ModulInhaltWoche (ModulInhalt_ID, Woche, Anzahl, Depot_ID) SELECT ModulInhalt.ID, Woche.Woche, 1, Depot.ID FROM ModulInhalt ,Depot ,Woche WHERE ModulInhalt.ID in (25,26,27,28,29) AND Depot.ID in  (19,12,15,6) AND Woche.Woche >= '2023.18' AND Woche.DonnerstagDesMonats=3;
+INSERT INTO ModulInhaltWoche (ModulInhalt_ID, Woche, Anzahl, Depot_ID) SELECT ModulInhalt.ID, Woche.Woche, 1, Depot.ID FROM ModulInhalt ,Depot ,Woche WHERE ModulInhalt.ID in (25,26,27,28,29) AND Depot.ID in  (13,11,4) AND Woche.Woche >= '2023.18' AND Woche.DonnerstagDesMonats=4;
+
+*/
