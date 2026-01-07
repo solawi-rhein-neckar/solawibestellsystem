0bbc67d8d1e5552a898a1d985c9bac08978fdf67
Florian Jostock <fjgh@regenduft.de>
2026-01-07 11:28:42 +0100

    date calculation wrong because javascript seems to use sonday as first day of next week

--- js/util.js
+++ js/util.js
@@ -116,7 +116,7 @@ Date.prototype.getWeekYear = function() {
 function weekToDate(yearWeekSeparatedByDot, dayOfWeek) {
     var year = Math.floor(yearWeekSeparatedByDot);
     var week = (yearWeekSeparatedByDot - year) * 100;
-    var date = new Date(year, 0, 4, 12, 0, 0);
+    var date = new Date(year, 0, 3, 12, 0, 0);
     return new Date(date.getTime() + ((week-1) * 86400000 * 7) + ((dayOfWeek - date.getDay()) * 86400000));
 }
 
